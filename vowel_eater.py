ff5213ff374a71ad08e36ea4451a332967bb26b3
JustCelline <103420465+JustCelline@users.noreply.github.com>
2022-04-11 18:06:46 +0800

    Create vowel_eater.py

---
+++ vowel_eater.py
@@ -0,0 +1,17 @@
+# Prompt the user to enter a word
+user_word = input("Enter a word: ").upper() #converting the user's input to UPPER CASES as instructed
+
+for letter in user_word: 
+    # Complete the body of the for loop.
+    if letter == 'A':
+        continue
+    elif letter == 'E':
+        continue
+    elif letter == 'I':
+        continue
+    elif letter == 'O':
+        continue
+    elif letter == 'U':
+        continue
+    else: 
+        print(letter)
